22978bab2caf5329a5ffc1abc6e06a11419da491
Wallace Borges <94559389+wallacebhorges@users.noreply.github.com>
2023-04-28 19:30:42 -0300

    mais uma ves, ajuste

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <header class="navbar fixed-top flex-md-nowrap p-0 shadow">
             <div class="container-fluid">
                 <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">
-                    <img src="assets/logo.svg" alt="Open Banking" width="150" />
+                    <img ssrc="./assets/logo.svg" alt="Open Banking" width="150" />
                 </a>
                 <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
